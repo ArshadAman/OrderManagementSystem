980eaf68afe70747b1741f5c45da3c32d17b76cf
Arshad Aman <arshadaman202@gmail.com>
2023-06-10 22:09:40 +0530

    Modified Django Admin templates

--- oms/settings.py
+++ oms/settings.py
@@ -14,13 +14,14 @@ SECRET_KEY = 'django-insecure-85=+s2j9n(rnzz=9njr0)j5k_@v$fmi9ns(v3346f)1medfhl@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "64.227.160.252"]
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    'jazzmin',
+    # 'admin_interface',
+    # 'colorfield',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -29,8 +30,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'order',
     'masters',
-    "qao",
+    'qao',
 ]
+
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -244,9 +247,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'devxplore202@gmail.com'
-EMAIL_HOST_PASSWORD = 'klpgpkvzxstnfyzp'
-
+EMAIL_HOST_USER = 'omverma1810@gmail.com'
+EMAIL_HOST_PASSWORD = 'ziueaejjwxrywhia'
 
 JET_SIDE_MENU_COMPACT = True
 
@@ -282,260 +284,4 @@ JET_THEMES = [
         'title': 'Light Gray'
     }
 ]
-
-
-# Deployement Configuarations
-# from django.core.management.utils import get_random_secret_key
-# from pathlib import Path
-# import os
-# import sys
-# import dj_database_url
-
-# BASE_DIR = Path(__file__).resolve().parent.parent
-# SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
-
-# # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-
-# ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
-
-# INSTALLED_APPS = [
-#     'jazzmin',
-#     'django.contrib.admin',
-#     'django.contrib.auth',
-#     'django.contrib.contenttypes',
-#     'whitenoise.runserver_nostatic',
-#     'django.contrib.sessions',
-#     'django.contrib.messages',
-#     'django.contrib.staticfiles',
-#     'order',
-# ]
-
-# MIDDLEWARE = [
-#     'django.middleware.security.SecurityMiddleware',
-#     'django.contrib.sessions.middleware.SessionMiddleware',
-#     'django.middleware.common.CommonMiddleware',
-#     'whitenoise.middleware.WhiteNoiseMiddleware',
-#     'django.middleware.csrf.CsrfViewMiddleware',
-#     'django.contrib.auth.middleware.AuthenticationMiddleware',
-#     'django.contrib.messages.middleware.MessageMiddleware',
-#     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-# ]
-
-# ROOT_URLCONF = 'oms.urls'
-
-# TEMPLATES = [
-#     {
-#         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-#         'DIRS': [os.path.join(BASE_DIR, 'templates')],
-#         'APP_DIRS': True,
-#         'OPTIONS': {
-#             'context_processors': [
-#                 'django.template.context_processors.debug',
-#                 'django.template.context_processors.request',
-#                 'django.contrib.auth.context_processors.auth',
-#                 'django.contrib.messages.context_processors.messages',
-#             ],
-#         },
-#     },
-# ]
-
-# WSGI_APPLICATION = 'oms.wsgi.application'
-
-
-# DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
-# # DATABASES = {
-# #         "default": {
-# #             "ENGINE": "django.db.backends.sqlite3",
-# #             "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-# #         }
-# #     }
-
-# if DEVELOPMENT_MODE is True:
-#     DATABASES = {
-#         "default": {
-#             "ENGINE": "django.db.backends.sqlite3",
-#             "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-#         }
-#     }
-# elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
-#     if os.getenv("DATABASE_URL", None) is None:
-#         raise Exception("DATABASE_URL environment variable not defined")
-#     DATABASES = {
-#         "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
-#     }
-
-
-# # Password validation
-# # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
-
-# AUTH_PASSWORD_VALIDATORS = [
-#     {
-#         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-#     },
-#     {
-#         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-#     },
-#     {
-#         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-#     },
-#     {
-#         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-#     },
-# ]
-
-# PASSWORD_HASHERS = [
-#     'django.contrib.auth.hashers.PBKDF2PasswordHasher',
-#     'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
-#     'django.contrib.auth.hashers.Argon2PasswordHasher',
-#     'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
-#     'django.contrib.auth.hashers.BCryptPasswordHasher',
-#     'django.contrib.auth.hashers.SHA1PasswordHasher',
-#     'django.contrib.auth.hashers.MD5PasswordHasher',
-#     'django.contrib.auth.hashers.UnsaltedSHA1PasswordHasher',
-#     'django.contrib.auth.hashers.UnsaltedMD5PasswordHasher',
-#     'django.contrib.auth.hashers.CryptPasswordHasher',
-# ]
-
-
-# # Internationalization
-# # https://docs.djangoproject.com/en/4.2/topics/i18n/
-
-# LANGUAGE_CODE = 'en-us'
-
-# TIME_ZONE = 'UTC'
-
-# USE_I18N = True
-
-# USE_TZ = True
-
-
-# # Static files (CSS, JavaScript, Images)
-# # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-# STATIC_URL = '/static/'
-# MEDIA_URL = '/images/'
-
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static')
-# ]
-
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-# # Default primary key field type
-# # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
-
-# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-# # Jazzmin Configurarions
-# JAZZMIN_SETTINGS = {
-#     "site_title": "VERSUITALITY",
-#     "site_header": "VERSUITALITY",
-#     "site_brand": "VERSUITALITY ",
-#     "site_icon": "dashboard/logo.svg",
-#     # Add your own branding here
-#     "site_logo": "dashboard/logo.svg",
-#     "welcome_sign": "Welcome to the VERSUITALITY",
-#     # Copyright on the footer
-#     "copyright": "VERSUITALITY",
-#     "user_avatar": None,
-#     ############
-#     # Top Menu #
-#     ############
-#     # Links to put along the top menu
-#     "topmenu_links": [
-#         # Url that gets reversed (Permissions can be added)
-#         {"name": "VERSUITALITY", "url": "home", "permissions": ["auth.view_user"]},
-#         # model admin to link to (Permissions checked against model)
-#         {"model": "auth.User"},
-#     ],
-#     #############
-#     # Side Menu #
-#     #############
-#     # Whether to display the side menu
-#     "show_sidebar": True,
-#     # Whether to aut expand the menu
-#     "navigation_expanded": True,
-#     # Custom icons for side menu apps/models See https://fontawesome.com/icons?d=gallery&m=free&v=5.0.0,5.0.1,5.0.10,5.0.11,5.0.12,5.0.13,5.0.2,5.0.3,5.0.4,5.0.5,5.0.6,5.0.7,5.0.8,5.0.9,5.1.0,5.1.1,5.2.0,5.3.0,5.3.1,5.4.0,5.4.1,5.4.2,5.13.0,5.12.0,5.11.2,5.11.1,5.10.0,5.9.0,5.8.2,5.8.1,5.7.2,5.7.1,5.7.0,5.6.3,5.5.0,5.4.2
-#     # for the full list of 5.13.0 free icon classes
-#     "icons": {
-#         "auth": "fas fa-users-cog",
-#         "auth.user": "fas fa-user",
-#         "users.User": "fas fa-user",
-#         "auth.Group": "fas fa-users",
-#         "admin.LogEntry": "fas fa-file",
-#     },
-#     # # Icons that are used when one is not manually specified
-#     "default_icon_parents": "fas fa-chevron-circle-right",
-#     "default_icon_children": "fas fa-arrow-circle-right",
-#     #################
-#     # Related Modal #
-#     #################
-#     # Use modals instead of popups
-#     "related_modal_active": False,
-#     #############
-#     # UI Tweaks #
-#     #############
-#     # Relative paths to custom CSS/JS scripts (must be present in static files)
-#     # Uncomment this line once you create the bootstrap-dark.css file
-#     "custom_css": "styles.css",
-#     "custom_js": None,
-#     # Whether to show the UI customizer on the sidebar
-#     "show_ui_builder": False,
-#     ###############
-#     # Change view #
-#     ###############
-#     "changeform_format": "horizontal_tabs",
-#     # override change forms on a per modeladmin basis
-#     "changeform_format_overrides": {
-#         "auth.user": "collapsible",
-#         "auth.group": "vertical_tabs",
-#     },
-# }
-
-# JAZZMIN_SETTINGS["show_ui_builder"] = False
-
-# JAZZMIN_UI_TWEAKS = {
-#     "navbar_small_text": False,
-#     "footer_small_text": False,
-#     "body_small_text": False,
-#     "brand_small_text": False,
-#     "brand_colour": "#000",
-#     "accent": "accent-navy",
-#     "navbar": "navbar-warning navbar-light",
-#     "no_navbar_border": False,
-#     "navbar_fixed": False,
-#     "layout_boxed": False,
-#     "footer_fixed": False,
-#     "sidebar_fixed": False,
-#     "sidebar": "sidebar-light-navy",
-#     "sidebar_nav_small_text": False,
-#     "sidebar_disable_expand": False,
-#     "sidebar_nav_child_indent": False,
-#     "sidebar_nav_compact_style": False,
-#     "sidebar_nav_legacy_style": False,
-#     "sidebar_nav_flat_style": False,
-#     "theme": "litera",
-#     "dark_mode_theme": None,
-#     "button_classes": {
-#         "primary": "btn-outline-primary",
-#         "secondary": "btn-outline-secondary",
-#         "info": "btn-info",
-#         "warning": "btn-warning",
-#         "danger": "btn-danger",
-#         "success": "btn-success"
-#     }
-# }
-
-
-# # EMAIL CONFIGURATIONS
-
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = 'omverma1810@gmail.com'
-# EMAIL_HOST_PASSWORD = 'jywjohxajpbpcfms'
 
